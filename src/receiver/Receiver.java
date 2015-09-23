8269a7775b6394f5e6e4b7f14a60a9e73a2a7ea0
sgstyjy <feizhangchina@gmail.com>
2015-09-23 09:15:26 +0200

    build framework rightly

--- src/receiver/Receiver.java
+++ src/receiver/Receiver.java
@@ -1,10 +1,40 @@
 package receiver;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
 public class Receiver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+	       //server socket
+	       ServerSocket s = new ServerSocket(Constant.PORT1);
+	       System.out.println("The outputs are from the Server: ");    
+	       //the start time
+	       Date date = new Date();
+	       Long starttime = date.getTime();
+	       try 
+	       {
+	          // for(;;)                          
+	           //{
+	              //waitting for the connection from client
+	              Socket socket = s.accept();
+	              //start the thread
+	              new ReceiverThread(socket);
+	              
+	              s.close();
+	              //the end time
+	              Date date1 = new Date();
+	              Long endtime = date1.getTime();
+	              Long duration = endtime-starttime;
+	              System.out.println("The receive time of is:"+duration);
+	         //  }
+	       }
+	       finally 
+	       {
+	             return;
+	       }
 	}
 
 }
--- src/receiver/ReceiverThread.java
+++ src/receiver/ReceiverThread.java
@@ -1,5 +1,47 @@
 package receiver;
 
-public class ReceiverThread {
+import java.io.DataInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
+public class ReceiverThread extends Thread{
+	 //File IO
+    private DataInputStream sin;
+    private FileOutputStream sout;    
+    //private FileOutputStream sout2;    
+    public ReceiverThread(Socket clientSocket) throws IOException 
+    {          
+           sin = new DataInputStream(clientSocket.getInputStream());
+           sout = new FileOutputStream(Constant.FILE_OUT); 
+          // sout2 = new FileOutputStream(Constant.BLOCKNUM_OUT); 
+           //start thread
+           start(); 
+    }
+    //the thread main method
+    public void run() 
+    {
+    	byte[] bb = new byte[Constant.TRANSFER_BUFFER];
+		int receive_length = 0;
+		int receive_blocks =0;
+		int receivesum = 0;
+        try 
+        {
+           receive_length = sin.read(bb, 0, Constant.TRANSFER_BUFFER);
+           while(receive_length>0){
+        	   sout.write(bb, 0, receive_length);
+        	   sout.flush();
+        	   //sout2.write(b);
+        	   receive_blocks++;
+        	   receivesum += receive_length;
+        	   receive_length = sin.read(bb, 0, Constant.TRANSFER_BUFFER);
+           }
+           sin.close();
+           sout.close();
+           System.out.println("The blocks received are: "+receive_blocks);
+           System.out.println("The bytes received are: "+receivesum);
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+    }
 }
